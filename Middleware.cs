d8e9a51f2b62ab4449ab56f62809a373685d2147
Do7a54 <midodoha7171@gmail.com>
2023-05-28 16:01:18 +0300

    End Of ch12

---
+++ Middleware.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Platform
+{
+    public class QueryStringMiddleWare
+    {
+        private RequestDelegate? next;    // new Constructor
+        public QueryStringMiddleWare()
+        {
+            // do nothing
+        }   // Page 310
+
+        //private RequestDelegate next;   // Constructor   // Modified Page 310 
+
+        public QueryStringMiddleWare(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+        public async Task Invoke(HttpContext context)   // Invke Method
+        {
+            if (context.Request.Method == HttpMethods.Get
+            && context.Request.Query["custom"] == "true")
+            {
+                if (!context.Response.HasStarted)   // ??????
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                await context.Response.WriteAsync("Class-based Middleware \n");
+            }
+            if (next != null)
+            {
+                await next(context);
+            }    // Page 310
+        }
+    }
+
+    public class LocationMiddleware
+    {
+        private RequestDelegate next;
+        private MessageOptions options;
+        public LocationMiddleware(RequestDelegate nextDelegate,
+        IOptions<MessageOptions> opts)
+        {
+            next = nextDelegate;
+            options = opts.Value;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/location")
+            {
+                await context.Response
+                .WriteAsync($"{options.CityName}, {options.CountryName}");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }   // LocationMiddleware Class Added Page 314
+}
+// Defining Middleware Using a Class  -- Page 302
--- Program.cs
+++ Program.cs
@@ -1,5 +1,74 @@
+using Microsoft.Extensions.Options;
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Albany";
+});   // Page 312  -- Change the cityname from "new York"
+
 var app = builder.Build();
+
+app.UseMiddleware<LocationMiddleware>();  // Added Page 315
+
+//app.MapGet("/location", async (HttpContext context,
+//IOptions<MessageOptions> msgOpts) => {
+//    Platform.MessageOptions opts = msgOpts.Value;
+//    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
+//});   // Added Page 312  // Modified Page 315 becouse it added to middleware class
+
+
+
+((IApplicationBuilder)app).Map("/branch", branch =>
+{
+    branch.Run(new Platform.QueryStringMiddleWare().Invoke);
+    //branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+    ////branch.Use(async (HttpContext context, Func<Task> next) => {  // Page 310
+    //branch.Run(async (context) => {
+    //    await context.Response.WriteAsync($"Branch Middleware");
+    //     });
+});    // Creating Pipeline Branches  -- Page 307
+// Modefied Page 312
+
+
+
+app.Use(async (context, next) =>
+{
+    await next();
+    await context.Response
+    .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+});  // Adding new Middleware -- page 304
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/short")
+    {
+        await context.Response
+        .WriteAsync($"Request Short Circuited");
+    }
+    else
+    {
+        await next();
+    }
+});   // Short-Circuiting the Request Pipeline  -- Page 306
+
+
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Get
+    && context.Request.Query["custom"] == "true")
+    {
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Custom Middleware \n");
+    }
+    await next();
+});    // Add a custom middleWare Page 299   
+/// Modefied Page  307
+
+
+
+//app.UseMiddleware<Platform.QueryStringMiddleWare>();  // Connect the MiddleWare Classs  -- Page 304 // Modifeid Page 312
 
 app.MapGet("/", () => "Hello World!");
 
