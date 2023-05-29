f5f8e005fbac3276779bf24ef79b639ced8148c9
Do7a54 <midodoha7171@gmail.com>
2023-05-29 11:05:13 +0300

    Monday 29-5

--- Program.cs
+++ Program.cs
@@ -2,12 +2,17 @@
 using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<MessageOptions>(options => {
-    options.CityName = "Albany";
-});
+
+//builder.Services.Configure<MessageOptions>(options => {
+//    options.CityName = "Albany";
+//});  // Modified Page 320
 
 var app = builder.Build();
 
-app.UseMiddleware<LocationMiddleware>();
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<Population>();
+app.UseMiddleware<Capital>();
+app.Run(async (context) => {
+    await context.Response.WriteAsync("Terminal Middleware Reached");
+});  //Added page 320
+
 app.Run();
